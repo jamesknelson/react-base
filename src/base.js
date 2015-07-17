6f55d364b0a230849baa4833771198000eb24558
jamesknelson <james@jamesknelson.com>
2015-07-17 18:31:45 +0900

    rename to react-c

--- src/base.js
+++ src/base.js
@@ -1,7 +1,7 @@
 import invariant from 'invariant'
+import c from 'react-c'
 import callbackRegister from 'react-callback-register'
 import passthrough from 'react-passthrough'
-import prefixedClasses from 'react-prefixed-classes'
 
 
 base.on = callbackRegister.on
@@ -10,13 +10,13 @@ export default function base(prefix, options = {}) {
     invariant(!component.prototype.base, "@base must be applied to a class with no `base` property")
 
     callbackRegister(component)
-    prefixedClasses(prefix)(component)
+    c(prefix)(component)
     passthrough(options.passthrough)(component)
       
     component.prototype.base = function base({passthrough = {}, classes = {}, callbacks = true} = {}) {
       return Object.assign(
         passthrough === false ? {} : this.passthrough(passthrough),
-        {className: classes === false ? '' : this.classes(classes),
+        {className: classes === false ? '' : this.cRoot(classes),
          ref: 'base'},
         callbacks === false ? {} : this.callbacks
       )
